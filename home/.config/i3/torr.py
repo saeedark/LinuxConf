005466dee037a17aea637325051bcb257fa09c2f
PesarAmmehZA <saeed.ali.rahimi@gmail.com>
2020-02-15 23:54:15 +0330

    Create torr.py

---
+++ home/.config/i3/torr.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2.7
+#coding: utf-8
+
+#print "<span foreground=\"#FF0000\">".decode('utf-8') +' '.decode('utf-8')
+#print "<span foreground=\"#FFFF00\">".decode('utf-8') + ' '.decode('utf-8')
+#print('<span foreground=\"#00FF00\">'.decode('utf-8'))
+print('<span foreground=\"#00FF00\">'.decode('utf-8')+'that'.decode('utf-8'))
